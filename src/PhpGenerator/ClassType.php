798c3ccb484271a9a7baaaa348c212953790ed14
David Grudl <david@grudl.com>
2012-01-02 04:27:15 +0100

    Updated copyright notices for 2012. Oh yeah, it's so much useless commit. But Mayans claim this is the last one.

--- src/PhpGenerator/ClassType.php
+++ src/PhpGenerator/ClassType.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Nette Framework (http://nette.org)
  *
- * Copyright (c) 2004, 2011 David Grudl (http://davidgrudl.com)
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
  *
  * For the full copyright and license information, please view
  * the file license.txt that was distributed with this source code.
