f996452bc20fed098dcd0236d73284b4ffdc61df
David Grudl <david@grudl.com>
2015-04-01 19:21:38 +0200

    composer: removed --dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
 // invoked through the command: ../vendor/bin/tester .
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
-	echo 'Install Nette Tester using `composer update --dev`';
+	echo 'Install Nette Tester using `composer install`';
 	exit(1);
 }
 
