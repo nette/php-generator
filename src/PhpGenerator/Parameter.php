8326fa06c638e840fe641a2de8510080a3361dcb
David Grudl <david@grudl.com>
2017-08-16 17:13:58 +0200

    Parameter::setOptional() triggers deprecation notice

--- src/PhpGenerator/Parameter.php
+++ src/PhpGenerator/Parameter.php
@@ -80,6 +80,7 @@ final class Parameter
 	 */
 	public function setOptional(bool $state = true): self
 	{
+		trigger_error(__METHOD__ . '() is deprecated, use setDefaultValue()', E_USER_DEPRECATED);
 		$this->hasDefaultValue = $state;
 		return $this;
 	}
