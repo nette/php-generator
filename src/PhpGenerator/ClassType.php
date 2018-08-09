4193dfc5314423e96f6963b8a17d5d745de8a152
David Grudl <david@grudl.com>
2018-08-09 15:04:46 +0200

    ClassType: deep clone

--- src/PhpGenerator/ClassType.php
+++ src/PhpGenerator/ClassType.php
@@ -442,4 +442,13 @@ final class ClassType
 			}
 		}
 	}
+
+
+	public function __clone()
+	{
+		$clone = function ($item) { return clone $item; };
+		$this->consts = array_map($clone, $this->consts);
+		$this->properties = array_map($clone, $this->properties);
+		$this->methods = array_map($clone, $this->methods);
+	}
 }
