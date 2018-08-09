febcc41779e33313dc119942052162cd0bed2b0a
David Grudl <david@grudl.com>
2018-08-09 16:56:20 +0200

    PhpNamespace: $keyword => const KEYWORDS

--- src/PhpGenerator/PhpNamespace.php
+++ src/PhpGenerator/PhpNamespace.php
@@ -26,7 +26,7 @@ final class PhpNamespace
 {
 	use Nette\SmartObject;
 
-	private static $keywords = [
+	private const KEYWORDS = [
 		'string' => 1, 'int' => 1, 'float' => 1, 'bool' => 1, 'array' => 1, 'object' => 1,
 		'callable' => 1, 'iterable' => 1, 'void' => 1, 'self' => 1, 'parent' => 1,
 	];
@@ -122,7 +122,7 @@ final class PhpNamespace
 
 	public function unresolveName(string $name): string
 	{
-		if (isset(self::$keywords[strtolower($name)]) || $name === '') {
+		if (isset(self::KEYWORDS[strtolower($name)]) || $name === '') {
 			return $name;
 		}
 		$name = ltrim($name, '\\');
