77f104f5c3fc9f1ec966c198828c9241411404d6
David Grudl <david@grudl.com>
2019-10-08 14:07:03 +0200

    throwing exceptions from __toString() is allowed since PHP 7.4

--- src/PhpGenerator/Closure.php
+++ src/PhpGenerator/Closure.php
@@ -40,6 +40,9 @@ final class Closure
 		try {
 			return (new Printer)->printClosure($this);
 		} catch (\Throwable $e) {
+			if (PHP_VERSION_ID >= 70400) {
+				throw $e;
+			}
 			trigger_error('Exception in ' . __METHOD__ . "(): {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", E_USER_ERROR);
 		}
 	}
