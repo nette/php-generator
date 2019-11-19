2402e8a1528bebe7fa8c0750423ee7452f2459fc
David Grudl <david@grudl.com>
2019-11-19 22:45:54 +0100

    Dumper: refactoring

--- src/PhpGenerator/Dumper.php
+++ src/PhpGenerator/Dumper.php
@@ -29,22 +29,39 @@ final class Dumper
 	 */
 	public function dump($var): string
 	{
-		return $this->_dump($var);
+		return $this->dumpVar($var);
 	}
 
 
-	private function _dump(&$var, int $level = 0)
+	private function dumpVar(&$var, int $level = 0): string
 	{
 		if ($var instanceof PhpLiteral) {
 			return (string) $var;
 
-		} elseif (is_float($var)) {
-			return var_export($var, true);
-
 		} elseif ($var === null) {
 			return 'null';
 
-		} elseif (is_string($var) && (preg_match('#[^\x09\x20-\x7E\xA0-\x{10FFFF}]#u', $var) || preg_last_error())) {
+		} elseif (is_string($var)) {
+			return $this->dumpString($var);
+
+		} elseif (is_array($var)) {
+			return $this->dumpArray($var, $level);
+
+		} elseif (is_object($var)) {
+			return $this->dumpObject($var, $level);
+
+		} elseif (is_resource($var)) {
+			throw new Nette\InvalidArgumentException('Cannot dump resource.');
+
+		} else {
+			return var_export($var, true);
+		}
+	}
+
+
+	private function dumpString(string $var): string
+	{
+		if (preg_match('#[^\x09\x20-\x7E\xA0-\x{10FFFF}]#u', $var) || preg_last_error()) {
 			static $table;
 			if ($table === null) {
 				foreach (array_merge(range("\x00", "\x1F"), range("\x7F", "\xFF")) as $ch) {
@@ -58,90 +75,90 @@ final class Dumper
 				$table['"'] = '\"';
 			}
 			return '"' . strtr($var, $table) . '"';
+		}
 
-		} elseif (is_string($var)) {
-			return "'" . preg_replace('#\'|\\\\(?=[\'\\\\]|$)#D', '\\\\$0', $var) . "'";
+		return "'" . preg_replace('#\'|\\\\(?=[\'\\\\]|$)#D', '\\\\$0', $var) . "'";
+	}
 
-		} elseif (is_array($var)) {
-			$space = str_repeat("\t", $level);
 
-			static $marker;
-			if ($marker === null) {
-				$marker = uniqid("\x00", true);
+	private function dumpArray(array &$var, int $level): string
+	{
+		static $marker;
+		if ($marker === null) {
+			$marker = uniqid("\x00", true);
+		}
+		if (empty($var)) {
+			return '[]';
+
+		} elseif ($level > self::MAX_DEPTH || isset($var[$marker])) {
+			throw new Nette\InvalidArgumentException('Nesting level too deep or recursive dependency.');
+		}
+
+		$space = str_repeat("\t", $level);
+		$outInline = '';
+		$outWrapped = "\n$space";
+		$var[$marker] = true;
+		$counter = 0;
+
+		foreach ($var as $k => &$v) {
+			if ($k !== $marker) {
+				$item = ($k === $counter ? '' : $this->dumpVar($k, $level + 1) . ' => ') . $this->dumpVar($v, $level + 1);
+				$counter = is_int($k) ? max($k + 1, $counter) : $counter;
+				$outInline .= ($outInline === '' ? '' : ', ') . $item;
+				$outWrapped .= "\t$item,\n$space";
 			}
-			if (empty($var)) {
-				$out = '';
+		}
 
-			} elseif ($level > self::MAX_DEPTH || isset($var[$marker])) {
-				throw new Nette\InvalidArgumentException('Nesting level too deep or recursive dependency.');
+		unset($var[$marker]);
+		$wrap = strpos($outInline, "\n") !== false || strlen($outInline) > self::WRAP_LENGTH - $level * self::INDENT_LENGTH;
+		return '[' . ($wrap ? $outWrapped : $outInline) . ']';
+	}
 
-			} else {
-				$out = '';
-				$outWrapped = "\n$space";
-				$var[$marker] = true;
-				$counter = 0;
-				foreach ($var as $k => &$v) {
-					if ($k !== $marker) {
-						$item = ($k === $counter ? '' : $this->_dump($k, $level + 1) . ' => ') . $this->_dump($v, $level + 1);
-						$counter = is_int($k) ? max($k + 1, $counter) : $counter;
-						$out .= ($out === '' ? '' : ', ') . $item;
-						$outWrapped .= "\t$item,\n$space";
-					}
-				}
-				unset($var[$marker]);
-			}
-			$wrap = strpos($out, "\n") !== false || strlen($out) > self::WRAP_LENGTH - $level * self::INDENT_LENGTH;
-			return '[' . ($wrap ? $outWrapped : $out) . ']';
 
-		} elseif ($var instanceof \Serializable) {
-			$var = serialize($var);
-			return 'unserialize(' . $this->_dump($var, $level) . ')';
+	private function dumpObject(&$var, int $level): string
+	{
+		if ($var instanceof \Serializable) {
+			return 'unserialize(' . $this->dumpString(serialize($var)) . ')';
 
 		} elseif ($var instanceof \Closure) {
 			throw new Nette\InvalidArgumentException('Cannot dump closure.');
-
-		} elseif (is_object($var)) {
-			$class = get_class($var);
-			if ((new \ReflectionObject($var))->isAnonymous()) {
-				throw new Nette\InvalidArgumentException('Cannot dump anonymous class.');
-
-			} elseif (in_array($class, ['DateTime', 'DateTimeImmutable'], true)) {
-				return $this->format("new $class(?, new DateTimeZone(?))", $var->format('Y-m-d H:i:s.u'), $var->getTimeZone()->getName());
-			}
-
-			$arr = (array) $var;
-			$space = str_repeat("\t", $level);
-
-			static $list = [];
-			if ($level > self::MAX_DEPTH || in_array($var, $list, true)) {
-				throw new Nette\InvalidArgumentException('Nesting level too deep or recursive dependency.');
-
-			} else {
-				$out = "\n";
-				$list[] = $var;
-				if (method_exists($var, '__sleep')) {
-					foreach ($var->__sleep() as $v) {
-						$props[$v] = $props["\x00*\x00$v"] = $props["\x00$class\x00$v"] = true;
-					}
-				}
-				foreach ($arr as $k => &$v) {
-					if (!isset($props) || isset($props[$k])) {
-						$out .= "$space\t" . $this->_dump($k, $level + 1) . ' => ' . $this->_dump($v, $level + 1) . ",\n";
-					}
-				}
-				array_pop($list);
-				$out .= $space;
-			}
-			return $class === 'stdClass'
-				? "(object) [$out]"
-				: __CLASS__ . "::createObject('$class', [$out])";
-
-		} elseif (is_resource($var)) {
-			throw new Nette\InvalidArgumentException('Cannot dump resource.');
-
-		} else {
-			return var_export($var, true);
 		}
+
+		$class = get_class($var);
+		if ((new \ReflectionObject($var))->isAnonymous()) {
+			throw new Nette\InvalidArgumentException('Cannot dump anonymous class.');
+
+		} elseif (in_array($class, ['DateTime', 'DateTimeImmutable'], true)) {
+			return $this->format("new $class(?, new DateTimeZone(?))", $var->format('Y-m-d H:i:s.u'), $var->getTimeZone()->getName());
+		}
+
+		$arr = (array) $var;
+		$space = str_repeat("\t", $level);
+
+		static $list = [];
+		if ($level > self::MAX_DEPTH || in_array($var, $list, true)) {
+			throw new Nette\InvalidArgumentException('Nesting level too deep or recursive dependency.');
+		}
+
+		$out = "\n";
+		$list[] = $var;
+		if (method_exists($var, '__sleep')) {
+			foreach ($var->__sleep() as $v) {
+				$props[$v] = $props["\x00*\x00$v"] = $props["\x00$class\x00$v"] = true;
+			}
+		}
+
+		foreach ($arr as $k => &$v) {
+			if (!isset($props) || isset($props[$k])) {
+				$out .= "$space\t" . $this->dumpVar($k, $level + 1) . ' => ' . $this->dumpVar($v, $level + 1) . ",\n";
+			}
+		}
+
+		array_pop($list);
+		$out .= $space;
+		return $class === 'stdClass'
+			? "(object) [$out]"
+			: __CLASS__ . "::createObject('$class', [$out])";
 	}
 
 
@@ -191,7 +208,7 @@ final class Dumper
 	private function formatMember($name): string
 	{
 		return $name instanceof PhpLiteral || !Helpers::isIdentifier($name)
-			? '{' . $this->_dump($name) . '}'
+			? '{' . $this->dumpVar($name) . '}'
 			: $name;
 	}
 
