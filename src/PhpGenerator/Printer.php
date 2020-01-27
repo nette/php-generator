0ef044403c6da6cfa596bdd7f5163bcc5c699e62
David Grudl <david@grudl.com>
2020-01-27 11:56:22 +0100

    Printer: added printType()

--- src/PhpGenerator/Printer.php
+++ src/PhpGenerator/Printer.php
@@ -122,7 +122,7 @@ class Printer
 		foreach ($class->getProperties() as $property) {
 			$type = $property->getType();
 			$def = (($property->getVisibility() ?: 'public') . ($property->isStatic() ? ' static' : '') . ' '
-				. ($type ? ($property->isNullable() ? '?' : '') . ($this->resolveTypes && $namespace ? $namespace->unresolveName($type) : $type) . ' ' : '')
+				. ltrim($this->printType($type, $property->isNullable(), $namespace) . ' ')
 				. '$' . $property->getName());
 
 			$properties[] = Helpers::formatDocComment((string) $property->getComment())
@@ -248,7 +248,7 @@ class Printer
 		foreach ($list as $param) {
 			$variadic = $function->isVariadic() && $param === end($list);
 			$type = $param->getType();
-			$params[] = ($type ? ($param->isNullable() ? '?' : '') . ($this->resolveTypes && $namespace ? $namespace->unresolveName($type) : $type) . ' ' : '')
+			$params[] = ltrim($this->printType($type, $param->isNullable(), $namespace) . ' ')
 				. ($param->isReference() ? '&' : '')
 				. ($variadic ? '...' : '')
 				. '$' . $param->getName()
@@ -261,13 +261,21 @@ class Printer
 	}
 
 
+	public function printType(?string $type, bool $nullable = false, PhpNamespace $namespace = null): string
+	{
+		return $type
+			? ($nullable ? '?' : '') . ($this->resolveTypes && $namespace ? $namespace->unresolveName($type) : $type)
+			: '';
+	}
+
+
 	/**
 	 * @param Closure|GlobalFunction|Method  $function
 	 */
-	protected function printReturnType($function, ?PhpNamespace $namespace): string
+	private function printReturnType($function, ?PhpNamespace $namespace): string
 	{
-		return $function->getReturnType()
-			? ': ' . ($function->isReturnNullable() ? '?' : '') . ($this->resolveTypes && $namespace ? $namespace->unresolveName($function->getReturnType()) : $function->getReturnType())
+		return ($tmp = $this->printType($function->getReturnType(), $function->isReturnNullable(), $namespace))
+			? ': ' . $tmp
 			: '';
 	}
 }
