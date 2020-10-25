03e766dbb9fbcb9b1fa9c7979bd340535456882c
David Grudl <david@grudl.com>
2020-10-25 17:23:59 +0100

    updated nette/coding-standard

---
+++ ecs.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Rules for Nette Coding Standard
+ * https://github.com/nette/coding-standard
+ */
+
+declare(strict_types=1);
+
+
+return function (Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
+	$containerConfigurator->import(PRESET_DIR . '/php71.php');
+
+	$parameters = $containerConfigurator->parameters();
+
+	$parameters->set('skip', [
+		// constant NULL, FALSE
+		PhpCsFixer\Fixer\Casing\LowercaseConstantsFixer::class => [
+			'src/PhpGenerator/Type.php',
+		],
+	]);
+};
