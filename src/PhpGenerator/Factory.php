98681d3b5503ca3089e9d06b2b2b3643ca211f9d
David Grudl <david@grudl.com>
2022-10-04 01:49:13 +0200

    cs

--- src/PhpGenerator/Factory.php
+++ src/PhpGenerator/Factory.php
@@ -70,7 +70,7 @@ final class Factory
 		}
 
 		$class->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
-		$class->setAttributes(self::getAttributes($from));
+		$class->setAttributes($this->getAttributes($from));
 		if ($from->getParentClass()) {
 			$class->setExtends($from->getParentClass()->name);
 			$class->setImplements(array_diff($class->getImplements(), $from->getParentClass()->getInterfaceNames()));
@@ -160,7 +160,7 @@ final class Factory
 		$method->setReturnReference($from->returnsReference());
 		$method->setVariadic($from->isVariadic());
 		$method->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
-		$method->setAttributes(self::getAttributes($from));
+		$method->setAttributes($this->getAttributes($from));
 		$method->setReturnType((string) $from->getReturnType());
 
 		return $method;
@@ -177,7 +177,7 @@ final class Factory
 			$function->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
 		}
 
-		$function->setAttributes(self::getAttributes($from));
+		$function->setAttributes($this->getAttributes($from));
 		$function->setReturnType((string) $from->getReturnType());
 
 		if ($withBody) {
@@ -196,8 +196,8 @@ final class Factory
 	{
 		$ref = Nette\Utils\Callback::toReflection($from);
 		return $ref instanceof \ReflectionMethod
-			? self::fromMethodReflection($ref)
-			: self::fromFunctionReflection($ref);
+			? $this->fromMethodReflection($ref)
+			: $this->fromFunctionReflection($ref);
 	}
 
 
@@ -226,7 +226,7 @@ final class Factory
 			$param->setNullable($param->isNullable() && $param->getDefaultValue() !== null);
 		}
 
-		$param->setAttributes(self::getAttributes($from));
+		$param->setAttributes($this->getAttributes($from));
 		return $param;
 	}
 
@@ -238,7 +238,7 @@ final class Factory
 		$const->setVisibility($this->getVisibility($from));
 		$const->setFinal(PHP_VERSION_ID >= 80100 ? $from->isFinal() : false);
 		$const->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
-		$const->setAttributes(self::getAttributes($from));
+		$const->setAttributes($this->getAttributes($from));
 		return $const;
 	}
 
@@ -248,7 +248,7 @@ final class Factory
 		$const = new EnumCase($from->name);
 		$const->setValue($from->getValue()->value ?? null);
 		$const->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
-		$const->setAttributes(self::getAttributes($from));
+		$const->setAttributes($this->getAttributes($from));
 		return $const;
 	}
 
@@ -265,7 +265,7 @@ final class Factory
 		$prop->setInitialized($from->hasType() && array_key_exists($prop->getName(), $defaults));
 		$prop->setReadOnly(PHP_VERSION_ID >= 80100 ? $from->isReadOnly() : false);
 		$prop->setComment(Helpers::unformatDocComment((string) $from->getDocComment()));
-		$prop->setAttributes(self::getAttributes($from));
+		$prop->setAttributes($this->getAttributes($from));
 		return $prop;
 	}
 
